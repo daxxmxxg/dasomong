c8384bc28976a6e0344cf2ad2ac659e2908d365d
dasomong <dasomong@gmail.com>
2018-01-13 14:03:12 +0900

    Add Right, Left, Up, Down Module Using Turtle

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("test")
+import turtle as t
+
+def Right():
+    t.setheading(0)
+    t.forward(20)
+
+def Left():
+    t.setheading(180)
+    t.forward(20)
+
+def Up():
+    t.setheading(90)
+    t.forward(20)
+
+def Down():
+    t.setheading(270)
+    t.forward(20)
+
+t.shape("turtle")
+t.speed(0)
+t.onkeypress(Right, "Right")
+t.listen()
+    
