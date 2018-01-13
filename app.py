ccf878ea0bfc0cfc2ab36dac6624d1ce743538cf
dasomong <dasomong@gmail.com>
2018-01-13 14:20:20 +0900

    Add Drawing Map Module

--- app.py
+++ app.py
@@ -16,8 +16,18 @@ def Down():
     t.setheading(270)
     t.forward(20)
 
+def Map():
+    
+
+# Main Start
+
+name = input("Please Typing your name: ")
+
 t.shape("turtle")
 t.speed(0)
 t.onkeypress(Right, "Right")
+t.onkeypress(Left, "Left")
+t.onkeypress(Up, "Up")
+t.onkeypress(Down, "Down")
 t.listen()
     
