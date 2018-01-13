7991273b47191cf2f84d342b4c413486541f67d9
dasomong <dasomong@gmail.com>
2018-01-13 14:36:59 +0900

    Draw Sample map

--- maze.py
+++ maze.py
@@ -5,3 +5,36 @@ map.width(5)
 map.hideturtle()
 map.speed(0)
 
+def draw_Map_of_Maze(t):
+    for i in range(3):
+        t.pendown()
+        t.forward(150)
+        t.penup()
+        t.forward(30)
+        t.pendown()
+        t.forward(150)
+        t.right(80)
+        t.forward(100)
+        t.right(80)
+  t.right(80)
+  t.forward(50)
+  t.left(80)
+  t.forward(30)
+  t.penup()
+  t.forward(40)
+  t.pendown()
+  t.forward(200)
+  t.penup()
+  t.forward(40)
+  t.pendown()
+  t.forward(30)
+
+colors = ["red", "blue", "yellow"]
+y = 150
+
+for color in colors:
+    map.color(color)
+    map.penup()
+    map.goto(-180,y)
+    draw_Map_of_Maze(map)
+    y = y - 100
